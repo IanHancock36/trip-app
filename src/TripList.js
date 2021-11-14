caf4448a6f081f5b8bfe7743ae873c61ba03d48b
Ian Hancock <northroadian@gmail.com>
2021-11-14 14:53:23 -0600

    trips

--- src/TripList.js
+++ src/TripList.js
@@ -3,13 +3,22 @@ import React, { useState, useEffect} from 'react'
 export default function TripList() {
     const [trips, setTrips] = useState([])
     console.log(trips)
-
+  useEffect(()=> {
     fetch(" http://localhost:3000/trips")
-        .then(response => response.json())
-        .then(json => setTrips(json))
+    .then(response => response.json())
+    .then(json => setTrips(json))
+}, [])
     return (
         <div>
             <h2>Trips</h2>
+            <ul>
+                {trips.map(trip =>(
+                   <li key={trip.id}>
+                       <h3>{trip.title}</h3>
+                       <p>{trip.price}</p>
+                   </li> 
+                ))}
+            </ul>
         </div>
     )
 }
