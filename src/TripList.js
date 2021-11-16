2ef547a8a7d92b8a995c457a08969e4197c9fe4e
Ian Hancock <northroadian@gmail.com>
2021-11-15 21:55:21 -0600

    query

--- src/TripList.js
+++ src/TripList.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect} from 'react'
 import './TripList.css'
 export default function TripList() {
     const [trips, setTrips] = useState([])
+    const [url , setUrl] =useState("http://localhost:3000/trips")
     console.log(trips)
   useEffect(()=> {
-    fetch(" http://localhost:3000/trips")
+    fetch(url)
     .then(response => response.json())
     .then(json => setTrips(json))
-}, [])
+}, [url])
     return (
         <div className='trip-list'>
             <h2>Trips</h2>
@@ -19,6 +20,14 @@ export default function TripList() {
                    </li> 
                 ))}
             </ul>
+            <div>
+                <button onClick={()=> setUrl("http://localhost:3000/trips?loc=us")}>
+                    Us Trip
+                </button>
+                <button>
+                 All Trips 
+                </button>
+            </div>
         </div>
     )
 }
