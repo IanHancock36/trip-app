fc0f11845a8ab53a272f1d1a3c0c4dbb8c5e2679
Ian Hancock <northroadian@gmail.com>
2021-11-18 06:32:55 -0600

    isPending

--- src/TripList.js
+++ src/TripList.js
@@ -3,11 +3,12 @@ import './TripList.css'
 import { useFetch } from './hooks/useFetch'
 export default function TripList() {
     const [url , setUrl] = useState("http://localhost:3000/trips")
-    const {data :trips} = useFetch(url)
+    const {data :trips ,isPending} = useFetch(url)
     // directly above you can call the data something else long as you specify
     return (
         <div className='trip-list'>
             <h2>Trips</h2>
+            {isPending && <div>Loading Trips</div>}
             <ul className='trip-list ul'>
                 {/* using trips && this will let it run the map without error if there is trips then it will render the map function. and if not there will not be an error */}
                 {trips && trips.map(trip =>(
